827ab54db6c5a6892d388b28d840320b0216854e
mahmudalvee <mahmud2.alvee@gmail.com>
2022-11-09 18:37:51 +0600

    home page

--- src/Pages/Home/Home/Appointment/Appointment.js
+++ src/Pages/Home/Home/Appointment/Appointment.js
@@ -1,21 +1,50 @@
 import React from "react";
 
-const APpointment = () => {
+const Appointment = () => {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero py-8 bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+          <h1 className="text-4xl font-bold">Get Appointment!</h1>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <span className="label-text">Phone</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="phone number"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <span className="label-text">Reason for Appointment</span>
+                </label>
+                <textarea className="textarea textarea-secondary" placeholder="Reason"></textarea>
+                
+                <label className="label">
+                  <span className="label-text">Date for Appointment</span>
+                </label>
+                <input
+                  type="date"
+                  placeholder="Date"
+                  className="input input-bordered"
+                />
+              
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-outline btn-primary btn-xs btn-md btn glass">Book Appointment</button>
+              </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default APpointment;
+export default Appointment;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import About from './About/About';
+import Appointment from './Appointment/Appointment';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <About></About>
+            <Appointment></Appointment>
         </div>
     );
 };
